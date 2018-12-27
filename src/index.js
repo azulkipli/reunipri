a94cd07696c8cea4fc1ac6cb1c92d1f8ffed795c
azul <azulkipli@gmail.com>
2018-12-27 11:25:35 +0700

    register

--- src/index.js
+++ src/index.js
@@ -28,6 +28,4 @@ render(App);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  serviceWorker.register();
-}
+serviceWorker.register();
